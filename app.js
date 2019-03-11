4e46e3dd771e8b26a4aa34cef117fa309a80048b
chithang3112 <chithang@pro-seeds.co.jp>
2019-03-11 14:56:35 +0900

    asd

--- app.js
+++ app.js
@@ -11,17 +11,20 @@ var httpServer = http.createServer(app);
 
 let PORT = process.env.PORT || 80;
 
-let discordApiUrl = process.env.DISCORD_API_URL || 'https://discordapp.com/api';
+let discordApiUrl = process.env.DISCORD_API_URL || 'https://discordapp.com/api/v6';
 
 let discordServerId = process.env.DISCORD_SEVER_ID || '545425833351839771';
 
+const CLIENT_ID = '554486406190333963';
+
+const SECRET_CLIENT_ID = 'mq4taSG_zO04d87vE5UrSJcK9HezzFbo';
+
 app.get('/',function(req, res){
 
-    console.log(res);
-    res.status(200).send();
+        console.log(req.query.code);
 });
 
-app.get(discordApiUrl + '/users/@me',function(req, res){
+app.post(discordApiUrl + '/users/@me',function(req, res){
     // var url = discordApiUrl + '/users/@me';
     console.log(res);
     res.status(200).send();
