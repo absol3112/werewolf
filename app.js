9b72f392cb3dd2f9a98adbfd15e2ef8bb85cf823
chithang3112 <chithang@pro-seeds.co.jp>
2019-03-11 15:22:58 +0900

    asd

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ let discordServerId = process.env.DISCORD_SEVER_ID || '545425833351839771';
 
 const CLIENT_ID = '554486406190333963';
 
-const SECRET_CLIENT_ID = 'mq4taSG_zO04d87vE5UrSJcK9HezzFbo';
+const CLIENT_SECRET = 'mq4taSG_zO04d87vE5UrSJcK9HezzFbo';
 
 app.get('/',function(req, res){
     var code = req.query.code;
-    exchange_code(code);
+    // exchange_code(code);
     options = {
         url: discordApiUrl +'/oauth2/token',
         data : {
